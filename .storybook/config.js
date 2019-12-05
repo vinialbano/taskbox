7bf74d0d94cfa8fe5fed1aa74e12e01be9133e45
Vinicius Albano <vinialbano@gmail.com>
2019-12-05 12:24:21 -0300

    taskbox UI

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import { configure } from '@storybook/vue';
+
+import '../src/index.css';
+
+const req = require.context('../src', true, /\.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+module.exports = api => ({
+  presets: ["@vue/app"],
+  ...(api.env("test") && { plugins: ["require-context-hook"] })
+});
